9e149f02aa307396d59dbda6a334470d6a124ebf
yuki-js <me@aoki.app>
2025-04-19 20:06:49 +0900

    vibe...

--- platform/wab/src/wab/server/db/custom-scripts/re-identify-users.ts
+++ platform/wab/src/wab/server/db/custom-scripts/re-identify-users.ts
@@ -2,6 +2,7 @@ import { DbMgr, SUPER_USER } from "@/wab/server/db/DbMgr";
 import { makeUserTraits } from "@/wab/server/routes/util";
 import { createInstance, Identify } from "@amplitude/analytics-node";
 import { EntityManager } from "typeorm";
+import { appConfig } from "../../nfigure-config";
 
 export async function reIdentifyUsers(em: EntityManager) {
   const dbMgr = new DbMgr(em, SUPER_USER);
@@ -9,7 +10,7 @@ export async function reIdentifyUsers(em: EntityManager) {
 
   console.log(`Identifying ${users.length} users...`);
   const amplitude = createInstance();
-  amplitude.init(process.env.AMPLITUDE_API_KEY!);
+  amplitude.init(appConfig.amplitudeApiKey!);
   for (const user of users) {
     const identify = new Identify();
     for (const [key, value] of Object.entries(makeUserTraits(user))) {
