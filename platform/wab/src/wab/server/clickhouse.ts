6c947420dbbea8c3a0eb81d8dd8e4cc9ad8d042b
yuki-js <me@aoki.app>
2025-04-19 22:58:00 +0900

    fix clickhouse

--- platform/wab/src/wab/server/clickhouse.ts
+++ platform/wab/src/wab/server/clickhouse.ts
@@ -2,16 +2,11 @@ import { createClient } from "@clickhouse/client";
 import { appConfig } from "./nfigure-config";
 
 export const getClickHouseConnection = () => {
-  const clickhouseConfig = appConfig.clickhouse ?? {
-    user: "admin",
-    pass: "",
-    db: "posthog",
-  };
   const client = createClient({
-    host: "http://clickhouse-db.plasmic.app",
-    username: clickhouseConfig.user,
-    password: clickhouseConfig.pass,
-    database: clickhouseConfig.db,
+    host: appConfig.clickhouse.host,
+    username: appConfig.clickhouse.username,
+    password: appConfig.clickhouse.password,
+    database: appConfig.clickhouse.database,
   });
   return client;
 };
